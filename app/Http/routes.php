9bc95d3104fd7d01f5fd7613f8890209b64c4a07
somebody <somebody@smd.com>
2018-05-18 20:29:27 +0300

    18-05-2018

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,25 +1,44 @@
 <?php
 
-  use App\Task;
-  use Illuminate\Http\Request;
+use App\Task;
+use Illuminate\Http\Request;
 
-  /**
-   * Вывести панель с задачами
-   */
-  Route::get('/', function () {
-    return view('tasks');
-  });
+/**
+ * Вывести панель с задачами
+ */
+Route::get('/', function () {
+//    $task= new Task();
+//    $tasks=$task->all();
+    $tasks = Task::orderBy ('created_at', 'asc')->get();
+    return view('tasks', ['tasks'=>$tasks]);
+});
 
-  /**
-   * Добавить новую задачу
-   */
-  Route::post('/task', function (Request $request) {
-    //
-  });
+/**
+ * Добавить новую задачу
+ */
+Route::post('/task', function (Request $request) {
+    $validator = Validator::make($request->all(), [
+                'name' => 'required|max:255|min:6',
+    ]);
 
-  /**
-   * Удалить задачу
-   */
-  Route::delete('/task/{task}', function (Task $task) {
-    //
-  });
+    if ($validator->fails()) {
+        return redirect('/')
+                        ->withInput()
+                        ->withErrors($validator);
+    }
+    var_dump($request->name);
+    
+    $task=new Task();
+    $task->name=$request->name;
+    $task->save();
+    return redirect('/');
+            
+});
+
+/**
+ * Удалить задачу
+ */
+Route::delete('/task/{task}', function (Task $task) {
+   $task->delete();
+   return redirect('/');
+});
--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -31,6 +31,42 @@
         </div>
     </form>
 </div>
+@if (count($tasks) > 0)
+<div class="panel panel-default">
+    <div class="panel-heading">
+        Текущая задача
+    </div>
 
-<!-- TODO: Текущие задачи -->
+    <div class="panel-body">
+        <table class="table table-striped task-table">
+
+            <!-- Заголовок таблицы -->
+            <thead>
+            <th>Task</th>
+            <th>&nbsp;</th>
+            </thead>
+
+            <!-- Тело таблицы -->
+            <tbody>
+                @foreach ($tasks as $task)
+                <tr>
+                    <!-- Имя задачи -->
+                    <td class="table-text">
+                        <div>{{ $task->name }}</div>
+                    </td>
+
+                    <td>
+                        <form action="{{url('task/'.$task->id)}}" method="post">
+                            {{ csrf_field()}}
+                            {{method_field('delete')}}
+                        <button class="btn btn-danger"><i class="fa fa-trash"></i></button>
+                        </form>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
+@endif
 @endsection
